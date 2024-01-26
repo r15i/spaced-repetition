198c686e8a786bb266a1353180fcc9ff467a1a3b
r15i <55852422+r15i@users.noreply.github.com>
2024-01-26 18:14:59 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+calcola data una serie di parti quando è meglio studiare una di queste 
+
+ad esempio utilzzando una finestra di incremento esponenziale e avendo 3 parti di un esame si otterrà
+
+
+giorno 1 ripeto A 
+giorno 2 ripeto A B 
+giorno 3 ripeto B C (NON RIPETO A pechè la finestra di tempo raddopiiat)
+giorno 4 ripeto A C
+giorno 5 ripeto B 
+giorno 6 ripeto C 
---
+++ spaced_rep_backed.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta
+
+
+
+
+
+
+
+
+
+
+def calcola_date_ripetizione(parti_esame, aumento_finestra):
+    # faccio funzione che calcola un array con numpy
+    
+    return date_ripetizione
+
+# Esempio di utilizzo
+parti_esame = ["Parte A", "Parte B", "Parte C"]
+aumento_finestra = 2  # Incremento di 7 giorni per ogni parte
+
+date_ripetizione = calcola_date_ripetizione(parti_esame, aumento_finestra)
+
+""" # Stampare le date di ripetizione per ogni parte
+for data_ripetizione, parti_da_ripetere in date_ripetizione:
+    print(f"Giorno {data_ripetizione.strftime('%Y-%m-%d')}: Ripeto {', '.join(parti_da_ripetere)}")
+ """
